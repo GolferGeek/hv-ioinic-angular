b7a2499ba1a80a48690b761ca639caf8b12392ba
Nicholas <nicholasjweber@protonmail.com>
2023-02-21 13:08:55 -0600

    Added auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AuthModule } from '@auth0/auth0-angular'
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, AuthButtonComponent, UserProfileComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'golfer-geek.us.auth0.com',
+      clientId: 'DqDFsmpuW5RPgFzWkE9OR7XBsOATBxr1',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
